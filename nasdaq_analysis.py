b9afba083904b8783ed6d72520c3509d8d0b700e
melvin se <nierghost755@gmail.com>
2025-04-25 03:44:17 +0200

    Dependency error fix

--- nasdaq_analysis.py
+++ nasdaq_analysis.py
@@ -4,6 +4,7 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
+import pytz
 
 # --- Helper Functions ---
 def get_nasdaq_100_tickers():
